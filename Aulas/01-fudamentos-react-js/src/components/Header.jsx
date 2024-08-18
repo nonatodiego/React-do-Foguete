d85bcb1e5c7548a01ae637755005ccc60c452358
Diego <nonatodiego@hotmail.com>
2024-08-18 10:36:36 -0300

    Componente: Header

--- Aulas/01-fudamentos-react-js/src/components/Header.jsx
+++ Aulas/01-fudamentos-react-js/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css'
 
+import trapfeed from '../assets/trapfeed.svg'
+
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong> Trapstar Feed </strong>
+            <img src={trapfeed} alt="Logo tipo do trapstar " />
         </header>
     )
 }
