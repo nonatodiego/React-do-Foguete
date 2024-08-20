5bc32cd3b73c4a86a5df708b095766b2b7b467ed
Diego <nonatodiego@hotmail.com>
2024-08-19 22:34:52 -0300

    Componente:Sidebar

--- Aulas/01-fudamentos-react-js/src/App.jsx
+++ Aulas/01-fudamentos-react-js/src/App.jsx
@@ -1,7 +1,10 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 
 export function App() {
@@ -9,14 +12,19 @@ export function App() {
   return (
     <>
       <Header />
-      <Post 
-        author="esse é o primeiro post" 
-        content="essa é macão o brabo do nordeste"
-      />     
-      <Post 
-        author="esse é o segundo post" 
-        content="Vasco da gama e nada mais"
-      />     
+     <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+          author="esse é o primeiro post" 
+          content="essa é macão o brabo do nordeste"
+          />     
+          <Post 
+            author="esse é o segundo post" 
+            content="Vasco da gama e nada mais"
+          />    
+        </main>
+      </div> 
     </>
   )
 }
